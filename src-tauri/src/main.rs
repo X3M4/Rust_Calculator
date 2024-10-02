3befb7eb48a700c4c373f8f3a083c722785163bc
Chema <jofermo5@gmail.com>
2024-10-02 23:17:52 +0200

    Modify main.rs by adding a calculator function

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,17 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::command;
+use meval;
 fn main() {
   tauri::Builder::default()
       .run(tauri::generate_context!())
       .expect("error while running tauri application");
+}
+
+#[command]
+fn calculate(expression: String) -> String{
+  match meval::eval_str(&expression) {
+    Ok(result) => result.to_string(),
+    Err(_) => "Error".to_string(),
+  }
 }
